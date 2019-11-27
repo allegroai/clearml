5cae1e83840b0c78b1cbe55202106b1859d4e729
allegroai <>
2019-11-28 00:47:51 +0200

    Clean hyper-parameter example

--- examples/hyper_parameters_example.py
+++ examples/hyper_parameters_example.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import sys
+from argparse import ArgumentParser
 
 from absl import app
 from absl import flags
@@ -18,7 +19,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('echo', None, 'Text to echo.')
 flags.DEFINE_string('another_str', 'My string', 'A string', module_name='test')
 
-task = Task.init(project_name='examples', task_name='absl example')
+task = Task.init(project_name='examples', task_name='hyper-parameters example')
 
 flags.DEFINE_integer('echo3', 3, 'Text to echo.')
 flags.DEFINE_string('echo5', '5', 'Text to echo.', module_name='test')
@@ -41,10 +42,18 @@ parameters['new_param'] = 'this is new'
 parameters['float'] = '9.9'
 print(parameters)
 
+
 def main(_):
     print('Running under Python {0[0]}.{0[1]}.{0[2]}'.format(sys.version_info), file=sys.stderr)
     logging.info('echo is %s.', FLAGS.echo)
 
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--argparser_int_value', help='integer value', type=int, default=1)
+    parser.add_argument('--argparser_disabled', action='store_true', default=False, help='disables something')
+    parser.add_argument('--argparser_str_value', help='string value', default='a string')
+
+    args = parser.parse_args()
+
     app.run(main)

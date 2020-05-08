c1e77420859cf9f8f147b41f9305019f7e182134
allegroai <>
2020-05-08 22:14:58 +0300

    Update PyTorch example default epochs

--- examples/pytorch_tensorboard.py
+++ examples/pytorch_tensorboard.py
@@ -86,7 +86,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=2, metavar='N',
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')

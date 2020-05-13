9fc07a39ebe7741683c3aba22df4cf95d23e4f59
allegroai <>
2020-05-13 20:27:10 +0300

    Add Python 3.8 to the supported versions list

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: Apache Software License',
     ],
     keywords='trains development machine deep learning version control machine-learning machinelearning '

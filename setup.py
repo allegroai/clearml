97270b05f362821aaee9c53c84388a42c6b0cca9
allegroai <>
2021-04-12 22:59:09 +0300

    Add python 3.9 to the support table

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: Apache Software License',
     ],
     keywords='clearml trains development machine deep learning version control machine-learning machinelearning '

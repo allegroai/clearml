b036bbce2f1c1a585b5b21ad64a22679184e3bbb
pollfly <75068813+pollfly@users.noreply.github.com>
2021-12-23 14:07:42 +0200

    Update XGBoost example (#524)
    * Change dataset (load_boston is deprecated)
    * Black format
    * Add save_model

--- examples/frameworks/xgboost/xgboost_metrics.py
+++ examples/frameworks/xgboost/xgboost_metrics.py
@@ -1,24 +1,27 @@
 import xgboost as xgb
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 from clearml import Task
 
-task = Task.init(project_name='examples', task_name='xgboost metric auto reporting')
+task = Task.init(project_name="examples", task_name="xgboost metric auto reporting")
 
-X, y = load_boston(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=100
+)
 
 dtrain = xgb.DMatrix(X_train, label=y_train)
 dtest = xgb.DMatrix(X_test, label=y_test)
 
-params = {
-    'objective': 'reg:squarederror',
-    'eval_metric': 'rmse'
-}
+params = {"objective": "reg:squarederror", "eval_metric": "rmse"}
 
 bst = xgb.train(
-    params, dtrain, num_boost_round=100,
-    evals=[(dtrain, 'train'), (dtest, 'test')],
-    verbose_eval=0
+    params,
+    dtrain,
+    num_boost_round=100,
+    evals=[(dtrain, "train"), (dtest, "test")],
+    verbose_eval=0,
 )
+
+bst.save_model("best_model")

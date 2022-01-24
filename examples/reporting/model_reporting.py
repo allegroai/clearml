ba4af9205d53b0323642fc9d85710bc354e25caf
Revital <revital@allegro.ai>
2022-01-24 08:11:30 +0200

    model logging examples

---
+++ examples/reporting/model_reporting.py
@@ -0,0 +1,23 @@
+# ClearML - Example of manual model reporting
+from clearml import Task, OutputModel
+
+# Connecting ClearML with the current process,
+task = Task.init(project_name="examples", task_name="Model configuration example")
+
+# Create output model and connect it to the task
+output_model = OutputModel(task=task)
+
+labels = {"background": 0, "cat": 1, "dog": 2}
+output_model.update_labels(labels)
+
+# Connecting a dictionary of definitions for a specific model configuration
+model_config_dict = {
+    "float": 13.37,
+    "dict": {"sub_value": "string", "sub_integer": 11},
+    "list_of_ints": [1, 2, 3, 4],
+}
+output_model.update_design(config_dict=model_config_dict)
+model_url = "https://allegro-examples.s3.amazonaws.com/clearml-public-resources/v1.0/clearml-examples-open/newexamples/examples/pytorch%20lightning%20mnist%20example.fb969db720e241e5859d522aa5226b81/models/training.pt"
+
+# Manually log a model file, which will have the labels connected above
+output_model.update_weights(register_uri=model_url)

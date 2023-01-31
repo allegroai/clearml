eaa400fbcdb582757c9d92bfdfd99db79e3aa209
Revital <revital@allegro.ai>
2023-01-31 08:31:53 +0200

    edit

--- clearml/model.py
+++ clearml/model.py
@@ -1460,8 +1460,7 @@ class OutputModel(BaseModel):
         :param str register_uri: The URI of an already uploaded weights file. The URI must be valid. Specify
             ``register_uri`` or ``weights_filename``, but not both.
         :param int iteration: The iteration number.
-        :param bool update_comment: Update the model comment with the local weights file name (to maintain
-            provenance) (Optional)
+        :param bool update_comment: Update the model comment with the local weights file name (to maintain provenance) (Optional)
 
             - ``True`` - Update model comment (Default)
             - ``False`` - Do not update

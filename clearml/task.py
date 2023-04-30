c5578d2f8b34af9d9025f62aac11215969a7f9fd
revital <revital@allegro.ai>
2023-04-30 11:20:54 +0300

    Edit docstrings

--- clearml/task.py
+++ clearml/task.py
@@ -1411,7 +1411,7 @@ class Task(_Task):
         :param object mutable: The experiment component to connect. The object must be one of the following types:
 
           - argparse - An argparse object for parameters.
-          - dict - A dictionary for parameters. Note: the dict supports only keys of type `str`
+          - dict - A dictionary for parameters. Note: only keys of type `str` are supported.
           - TaskParameters - A TaskParameters object.
           - :class:`Model` - A model object for initial model warmup, or for model update/snapshot uploading. In practice the model should be either :class:`InputModel` or :class:`OutputModel`.
           - type - A Class type, storing all class properties (excluding '_' prefixed properties).

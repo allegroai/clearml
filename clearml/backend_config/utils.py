a02a114bd91e2f8f5767faf8bd64061c37eed85d
clearml <>
2025-01-08 18:35:35 +0200

    Black formatting

--- clearml/backend_config/utils.py
+++ clearml/backend_config/utils.py
@@ -11,12 +11,12 @@ if TYPE_CHECKING:
 
 
 def get_items(cls):
-    """ get key/value items from an enum-like class (members represent enumeration key/value) """
-    return {k: v for k, v in vars(cls).items() if not k.startswith('_')}
+    """get key/value items from an enum-like class (members represent enumeration key/value)"""
+    return {k: v for k, v in vars(cls).items() if not k.startswith("_")}
 
 
 def get_options(cls):
-    """ get options from an enum-like class (members represent enumeration key/value) """
+    """get options from an enum-like class (members represent enumeration key/value)"""
     return get_items(cls).values()
 
 
